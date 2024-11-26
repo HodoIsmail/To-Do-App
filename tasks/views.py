f5bc07cc7b5ba40a5695660d837a1cd99cc759db
Hodo Ismail <ic17oq5i@students.codeinstitute.net>
2024-11-26 23:42:44 +0000

    create index.html template

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from . models import Task
 
 # Create your views here.
 
-def index(request):
-    tasks = Task.objects.all()
-    context = {'tasks': tasks}
-    return render(request, 'index.html', context)
-# class TaskList(generic.ListView):
-#     model = Task
+# def index(request):
+#     tasks = Task.objects.all()
+#     context = {'tasks': tasks}
+#     return render(request, 'index.html', context)
+class TaskList(generic.ListView):
+    queryset = Task.objects.all()
+    template_name= "tasks/index.html"
+
+def task_details(request):
+    queryset = Task.objects.all()
+    task= get_object_or_404(queryset)
+    return render(request, 'tasks/index.html', {'task': task},)
