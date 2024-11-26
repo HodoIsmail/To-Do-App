842baeb5c5866f8f3f64b36dca5599e7f5488d59
Hodo Ismail <ic17oq5i@students.codeinstitute.net>
2024-11-26 16:19:37 +0000

    create task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tasks(request):
+    user = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="user"
+)
+    name = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    importance = models.IntegerField( choices= [(1,"low"), (2,"medium"), (3,"high")])
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
