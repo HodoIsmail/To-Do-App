33095ae343e39f66a1cb7aa061e52ccc6652c5f9
Hodo Ismail <ic17oq5i@students.codeinstitute.net>
2024-11-27 15:55:11 +0000

    create task form

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     # path('', views.index , name="home")
-      path('', views.TaskList.as_view(), name='task'),
+      path('', views.task_details , name='task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from . models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -8,11 +9,16 @@ from . models import Task
 #     tasks = Task.objects.all()
 #     context = {'tasks': tasks}
 #     return render(request, 'index.html', context)
-class TaskList(generic.ListView):
-    queryset = Task.objects.all()
-    template_name= "tasks/index.html"
+# class TaskList(generic.ListView):
+#     queryset = Task.objects.all()
+#     template_name= "tasks/index.html"
 
 def task_details(request):
     queryset = Task.objects.all()
     task= get_object_or_404(queryset)
-    return render(request, 'tasks/index.html', {'task': task},)
+    task_form = TaskForm()
+
+    return render(request, 
+    'tasks/index.html',
+     {'task': task},
+     {'task_form': task_form})
